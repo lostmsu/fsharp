17f7c71ea90a238ab33fbd70d79f4b0591be4d74
Victor Milovanov <lostfreeman@gmail.com>
2015-04-20 23:20:15 -0700

    + LiveReload Reload function

--- src/fsharp/fsi/LiveReload.fs
+++ src/fsharp/fsi/LiveReload.fs
@@ -3,10 +3,17 @@ namespace Microsoft.FSharp.Compiler.Interactive
 open System.IO
 open System.Collections.Generic
 
+type private LiveFile(path) =
+    let mutable isDirty = false
+    member this.Path = path
+    member this.IsDirty with get() = isDirty
+                        and internal set value = isDirty <- value
+
+[<Sealed>]
 type LiveReload() =
     let mutable disposed = false
     let watchers = dict []
-    let files = List<string>()
+    let files = List<LiveFile>()
 
     let watch directory =
         if watchers.ContainsKey directory then ()
@@ -21,11 +28,37 @@ type LiveReload() =
     member this.Track file =
         notDisposed()
 
-        if files.Contains file then ()
+        if files.Exists(fun liveFile -> liveFile.Path = file) then ()
         else
         let directory = Path.GetDirectoryName(file) |> Path.GetFullPath
-        files.Add file
+        files.Add <| LiveFile(file)
         try
             watch directory
         with e ->
             files.RemoveAt(files.Count - 1)
+
+    member this.Reload loader =
+        let dirtyStart = files.FindIndex(fun liveFile -> liveFile.IsDirty)
+        if dirtyStart < 0 then ()
+        else
+        let dirtyList = Array.init (files.Count - dirtyStart) (fun i -> files.[i + dirtyStart].Path)
+        let remainder = loader dirtyList
+        if Array.length remainder < dirtyList.Length then
+            raise <| System.ArgumentException("loader")
+
+        let di = dirtyList.Length - Array.length remainder
+        for i = Array.length remainder - 1 downto 0 do
+            if not <| dirtyList.[di + i].Equals(remainder.[i], System.StringComparison.OrdinalIgnoreCase) then
+                raise <| System.ArgumentException "loader"
+        
+        for i = dirtyStart to files.Count - 1 - remainder.Length do
+            files.[i].IsDirty <- false
+
+    interface System.IDisposable with
+        member this.Dispose() =
+            if disposed then ()
+            else
+            disposed <- true
+            files.Clear()
+            for watcher in watchers.Values do watcher.Dispose()
+            watchers.Clear()
